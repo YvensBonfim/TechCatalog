973a452772767997d8d13a79d69808a340db09bd
Yvens <98363275+YvensBonfim@users.noreply.github.com>
2026-04-06 11:27:46 -0300

    Initial commit

---
+++ src/main/java/com/yvens/techcatalog/TechcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.yvens.techcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechcatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yvens/techcatalog/TechcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yvens.techcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
